f7b78fa8895447436d8c92a79880c91dd747e2cf
chenshoumao <JJ@JJ-PC>
2017-09-26 18:26:19 +0800

    1111

--- src/com/solar/dao/impl/LandDaoImpl.java
+++ src/com/solar/dao/impl/LandDaoImpl.java
@@ -59,6 +59,15 @@ public class LandDaoImpl implements LandDao {
 	private final String DITU = "ditu";
 	private final String DITU_VALUE = "底图版本";
 
+	//压缩包名字
+	private static String fileName;
+	 
+	// 新版本（即岸端最新版本）
+	String newVersionPath;
+	
+	List<FileMd5> compareFile = null;
+	
+	
 	private static ResourceBundle resource = ResourceBundle.getBundle("config/land");
 
 	@Override
@@ -69,7 +78,7 @@ public class LandDaoImpl implements LandDao {
 		logger.debug("  先确定一个压缩包名字。。。");
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
-		String fileName = host + "_" + sdf.format(date) + ".zip";
+		fileName = host + "_" + sdf.format(date) + ".zip";
 		logger.debug("  压缩包名字是： " + fileName);
 		logger.debug("  声明连接oracle数据库的对象");
 		// 声明连接oracle数据库的对象
@@ -218,7 +227,9 @@ public class LandDaoImpl implements LandDao {
 				logger.debug(" 生成对应的增量包");
 				// 生成对应的增量包
 				boolean generateState = generateIncrement(host, key, fileName, moduleVersionOfShip, upToDateVersion);
-
+				 
+				
+				
 				if (generateState) {
 					// 到此步骤已经可以算是更新成功的了，将更新语句的状态设置为 1，1 代表成功的意思
 					logger.debug("  生产增量包 ：成功");
@@ -227,6 +238,15 @@ public class LandDaoImpl implements LandDao {
 					ps.setString(7, description);
 					ps.execute();
 					ps.close();
+					
+					logger.debug("  复制增量文件到临时目录！");
+					if(copyFile(key, fileName, moduleVersionOfShip, upToDateVersion, compareFile, newVersionPath)){
+						logger.debug(" 开始生成压缩包！");
+						generateZipFile(host,fileName);
+					}
+					 
+						Thread.sleep(1000);
+					 
 
 				} else {
 					logger.debug("  生产增量包 ：失败");
@@ -237,6 +257,7 @@ public class LandDaoImpl implements LandDao {
 				if (key != "db") {
 					Map<String, Object> mapInfo = new HashMap<String, Object>();
 					mapInfo.put(keyName, generateState);
+					mapInfo.put("zipName", fileName);
 					list.add(mapInfo);
 				}
 			}
@@ -268,12 +289,15 @@ public class LandDaoImpl implements LandDao {
 			} else {
 				// 走到这一步 就是打包都完成了，都没问题，然后就通知通道准备传输压缩文件
 				conn.commit();
-				insertInfoToChannel(host, "zipfile" + "/" + fileName);
+				//insertInfoToChannel(host, "zipfile" + "/" + fileName);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			throw new MyException("数据解析错误 ：" + e);
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -339,8 +363,9 @@ public class LandDaoImpl implements LandDao {
 			
 			
 			
-			String queryShipId = "select ship_id from CHANNEL_CONFIG where client_port = '" + host + "'";
+			String queryShipId = "select ship_id from CHANNEL_CONFIG where client_host = '" + host + "'";
 			oraPs = oraConn.prepareStatement(queryShipId);
+			oraRs = oraPs.executeQuery();
 			String ship_id = "";
 			if (oraRs.next()) {
 				ship_id = oraRs.getString(1);
@@ -422,7 +447,7 @@ public class LandDaoImpl implements LandDao {
 		// 旧版本（即船端的版本）
 		String oldVersionPath = bundleUtil.getInfo("config/land", key) + "/" + moduleVersionOfShip + "/";
 		// 新版本（即岸端最新版本）
-		String newVersionPath = bundleUtil.getInfo("config/land", key) + "/" + moduleVersionOfLand + "/";
+		newVersionPath = bundleUtil.getInfo("config/land", key) + "/" + moduleVersionOfLand + "/";
 		logger.debug("	两版本的位置：" + oldVersionPath + "," + newVersionPath);
 		try {
 			if (key.equals("db")) {
@@ -472,10 +497,10 @@ public class LandDaoImpl implements LandDao {
 			Map<String, FileMd5> newVersionMap = listDir(newVersionPath);
 			logger.debug("	比较两版本的文件，将增量结果储存在 compareFile 的集合中");
 			// 比较两版本的文件，将增量结果储存在 compareFile 的集合中
-			List<FileMd5> compareFile = compareFile(newVersionMap, oldVersionMap);
+			compareFile = compareFile(newVersionMap, oldVersionMap);
 			logger.debug("	复制增量到一个临时目录");
 			// 复制增量到一个临时目录
-			copyFile(host, key, fileName, moduleVersionOfShip, moduleVersionOfLand, compareFile, newVersionPath);
+			copyFile(key, fileName, moduleVersionOfShip, moduleVersionOfLand, compareFile, newVersionPath);
 			result = true;
 
 		} catch (IOException e) {
@@ -493,7 +518,7 @@ public class LandDaoImpl implements LandDao {
 	 * @param moduleVersionOfLand
 	 * @param moduleVersionOfShip
 	 */
-	public static void copyFile(String host, String key, String fileName, String moduleVersionOfShip,
+	public static boolean copyFile(String key, String fileName, String moduleVersionOfShip,
 			String moduleVersionOfLand, List<FileMd5> fileMd5s, String startTag) {
 		CopyFileUtil copyUtil = new CopyFileUtil();
 		ResourceBundleUtil bundleUtil = new ResourceBundleUtil();
@@ -509,16 +534,11 @@ public class LandDaoImpl implements LandDao {
 		boolean stateCopyResult = false;
 		for (FileMd5 fileMd5 : fileMd5s) {
 			String filePath = fileMd5.getFile().getAbsolutePath();
-			filePath = filePath.replaceAll("\\\\", "/");
-			// String startTag = "D:\\海图项目\\zip2";
-			// D:\海图项目\reposities\应用\1.0.0.1_app_release_20170718\web
-			// D:\海图项目\reposities\应用\1.0.0.1_app_release_20170718\web\hello.jsp
+			filePath = filePath.replaceAll("\\\\", "/"); 
 			String destDir = "";
 			String sourceDir = fileMd5.getFile().getAbsolutePath();
 			String temp = startTag;
-			String notApp = "";
-			// D:/海图项目/reposities/应用/1.0.0.1_app_release_20170718/config/3212.txt
-			// D:/海图项目/reposities/应用/1.0.0.1_app_release_20170718/
+			String notApp = ""; 
 			if (key.equals("app")) {
 				temp = startTag + "web";
 				sourceDir = sourceDir.replaceAll("\\\\", "/");
@@ -528,8 +548,7 @@ public class LandDaoImpl implements LandDao {
 				if (sourceDir.indexOf(startTag + "config") != -1) {
 					temp = startTag + "config";
 					notApp = "config";
-				} // D:\海图项目\reposities\应用\1.0.0.0_app_release_20170713\config
-					// D:\海图项目\reposities\应用\1.0.0.0_app_release_20170713\web\WEB-INF\classes
+				}  
 			} else {
 				notApp = key;
 			}
@@ -555,44 +574,61 @@ public class LandDaoImpl implements LandDao {
 				throw new MyException("		文件复制失败");
 			}
 		}
-		logger.debug("	创建一个文件 用来标明增量文件是存在于哪两个版本之间");
-		// 创建一个文件 用来标明增量文件是存在于哪两个版本之间
-		String divisionFilePath = tempPath + "/" + (key.equals("app") ? "division.txt" : key + "/division.txt");
-		logger.debug("		文件是" + divisionFilePath);
-		File file = new File(divisionFilePath);
-		WriteFileUtil writeFileUtil = new WriteFileUtil();
-		List list = new ArrayList();
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("from", moduleVersionOfShip);
-		map.put("to", moduleVersionOfLand);
-		list.add(map);
-		ObjectMapper mapper = new ObjectMapper();
-		String json;
-		try {
-			json = mapper.writeValueAsString(list);
-			writeFileUtil.writeInfoToFile(json, divisionFilePath, false);
-			logger.debug("	文件内容是" + json);
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			throw new MyException(e.toString());
+		
+		if (stateCopyResult) { 
+			logger.debug("	创建一个文件 用来标明增量文件是存在于哪两个版本之间");
+			// 创建一个文件 用来标明增量文件是存在于哪两个版本之间
+			String divisionFilePath = tempPath + "/" + (key.equals("app") ? "division.txt" : key + "/division.txt");
+			logger.debug("		文件是" + divisionFilePath);
+			File file = new File(divisionFilePath);
+			WriteFileUtil writeFileUtil = new WriteFileUtil();
+			List list = new ArrayList();
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("from", moduleVersionOfShip);
+			map.put("to", moduleVersionOfLand);
+			list.add(map);
+			ObjectMapper mapper = new ObjectMapper();
+			String json;
+			try {
+				json = mapper.writeValueAsString(list);
+				writeFileUtil.writeInfoToFile(json, divisionFilePath, false);
+				logger.debug("	文件内容是" + json);
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new MyException(e.toString());
+			} 
 		}
+		return stateCopyResult; 
 
+	}
+	
+	//生成一个增量包
+	public boolean generateZipFile(String host,String fileName){
 		logger.debug("岸端第四步");
 		logger.debug("	打包文件，形成一个增量压缩包");
 		// 打包文件，形成一个增量包
 		Zip zip = new Zip();
+		boolean flag = true;
+		try { 
 
-		String sourcePath = bundleUtil.getInfo("config/land", "tempPath") + File.separator
-				+ fileName.substring(0, fileName.indexOf(".zip"));
+			ResourceBundleUtil bundleUtil = new ResourceBundleUtil();
 
-		String outPutZipPath = ResourceBundleUtil.getInfo("config/land", "zipPath");
-		logger.debug("\t源文件路径：" + sourcePath);
-		logger.debug("\t增量包路径： " + outPutZipPath);
-		zip.zip(host, fileName, sourcePath, outPutZipPath);
-		logger.debug("	向管道的数据库输入数据");
-		logger.debug("  压缩包名是 ： " + fileName);
+			String sourcePath = bundleUtil.getInfo("config/land", "tempPath") + File.separator
+					+ fileName.substring(0, fileName.indexOf(".zip"));
 
+			String outPutZipPath = ResourceBundleUtil.getInfo("config/land", "zipPath");
+			logger.debug("\t源文件路径：" + sourcePath);
+			logger.debug("\t增量包路径： " + outPutZipPath);
+			zip.zip(host, fileName, sourcePath, outPutZipPath);
+			// logger.debug(" 向管道的数据库输入数据");
+			logger.debug("  压缩包名是 ： " + fileName);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			flag = false;
+		}
+		return flag;
 	}
 
 	/*
